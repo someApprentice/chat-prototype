9c54e467af60ea2921cd3c462a2e617f034218b5
someApprentice <someApprentice@openmailbox.org>
2017-06-28 08:48:37 +0300

    Small refactoring: handlers moved to main Cotroller; fixes;

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,24 +2,122 @@ function Controller(contacts, conversation) {
   this.contacts = contacts;
   this.conversation = conversation;
 
-  this.contacts.view.contactLinks.click(this, this.handleConversation);
+  this.contacts.view.searchbox.keydown(this.handleEnterKeyOnSearchForm.bind(this));
+  this.contacts.view.searchform.submit(this.handleSubmitOnSearchForm.bind(this));
+
+  this.contacts.view.contactList.click(this.contacts.view, this.contacts.view.turnCheckedClass);
+  this.contacts.view.contactLinks.click(this.handleClickOnContact);
+  this.contacts.view.contactLinks.mousedown(this, this.handleConversation);
+
+  this.conversation.view.messagebox.keydown(this.handleEnterKeyOnMessageForm.bind(this));
+  this.conversation.view.submit.click(this.handleClickOnMessageInput.bind(this));
+  this.conversation.view.messageform.submit(this.handleSubmitOnMessageForm.bind(this));
 }
+
+Controller.prototype.handleEnterKeyOnSearchForm = function(e) {
+  if (e.keyCode == 13) {
+    $(this.contacts.view.searchform).submit();
+  }
+};
+
+Controller.prototype.handleSubmitOnSearchForm = function(e) {
+  e.preventDefault();
+
+  var that = this;
+
+  var q = $(that.contacts.view.searchbox).val();
+
+  clearInterval(that.contacts.contactsInterval);
+
+  if (q != '') {
+    that.contacts.backend.searchContacts(q).then(function(data) {
+      that.contacts.view.showContacts(data);
+    });
+  } else {
+    that.refreshContacts();
+  }
+
+  return false;
+};
+
+Controller.prototype.handleClickOnContact = function(e) {
+  e.preventDefault();
+
+  var datawith = $(this).attr('data-with');
+
+  window.history.pushState({}, '', '/conversation.php?with=' + datawith);
+};
+
+Controller.prototype.refreshContacts = function() {
+  var that = this;
+
+  clearInterval(that.contacts.contactsInterval);
+
+  that.contacts.contactsInterval = setInterval(function() {
+    that.contacts.backend.getContacts().then(function(data) {
+      that.contacts.view.showContacts(data);
+
+      that.contacts.view.contactLinks.click(function(e) {
+        e.preventDefault();
+      });
+
+      that.contacts.view.contactList.mousedown(that.contacts.view, that.contacts.view.turnCheckedClass);
+      that.contacts.view.contactLinks.mousedown(that.handleClickOnContact);
+      that.contacts.view.contactLinks.mousedown(that, that.handleConversation);
+    });
+  }, 500);
+};
+
+Controller.prototype.handleEnterKeyOnMessageForm = function(e) {
+  if (e.ctrlKey && e.keyCode == 13) {
+    $(this.conversation.view.messagebox).val($(this.conversation.view.messagebox).val() + "\n");
+  } else if (e.keyCode == 13) {
+    e.preventDefault();
+
+    $(this.conversation.view.messageform).submit();
+    $(this.conversation.view.messagebox).val('');
+  }
+};
+
+Controller.prototype.handleClickOnMessageInput = function(e) {
+  e.preventDefault();
+
+  $(this.conversation.view.messageform).submit();
+  $(this.conversation.view.messagebox).val('');
+};
+
+Controller.prototype.handleSubmitOnMessageForm = function(e) {
+  e.preventDefault();
+
+  var to = $(this.conversation.view.messageform).attr('data-send-to'),
+      message =  $(this.conversation.view.messagebox).val(),
+      token = this.conversation.view.token;
+
+  this.conversation.backend.postMessage(to, message, token).then(function(data) {
+    // ...
+  });
+
+  return false;
+};
 
 Controller.prototype.handleConversation = function(e) {
   var controller = e.data;
 
   var datawith = $(this).attr('data-with');
 
-  controller.conversation.backend.getMessages(datawith).then(function(data) {
-    var newDataWith = $(this).attr('data-with');
+  var url = window.location.href;
 
-    if (newDataWith == datawith) {
+  controller.conversation.backend.getMessages(datawith).then(function(data) {
+    var actualUrl = window.location.href;
+
+    if (url == actualUrl) {
       controller.conversation.view.showMessageForm(datawith);
       controller.conversation.view.showMessages(data);
       controller.conversation.view.moveMessagesToBottom();
-      controller.conversation.view.scrollDownMessages(); 
+      controller.conversation.view.scrollDownMessages();  
     }
   });
+
   controller.conversation.refreshMessages(datawith);
 }
 
@@ -66,65 +164,14 @@ Backend.prototype.postMessage = function(to, message, token) {
   return promise;
 };
 
+
 function Contacts(backend, modelView, view) {
   this.backend = backend;
   this.modelView  = modelView;
   this.view = view;
 
   this.contactsInterval;
-
-  this.view.searchbox.keydown(this.handleEnterKeyOnSearchForm.bind(this));
-  this.view.searchform.submit(this.handleSubmitOnSearchForm.bind(this));
-
-  this.view.contactList.click(this.view, this.view.turnCheckedClass);
-  this.view.contactLinks.click(this.handleClickOnContact);
 }
-
-Contacts.prototype.handleEnterKeyOnSearchForm = function(e) {
-  if (e.keyCode == 13) {
-    $(this.view.searchform).submit();
-  }
-};
-
-Contacts.prototype.handleSubmitOnSearchForm = function(e) {
-  e.preventDefault();
-
-  var that = this;
-
-  var q = $(that.view.searchbox).val();
-
-  clearInterval(that.contactsInterval);
-
-  if (q != '') {
-    that.backend.searchContacts(q).then(function(data) {
-      that.view.showContacts(data);
-    });
-  } else {
-    that.refreshContacts();
-  }
-
-  return false;
-};
-
-Contacts.prototype.handleClickOnContact = function(e) {
-  e.preventDefault();
-
-  var datawith = $(this).attr('data-with');
-
-  window.history.pushState({}, '', '/conversation.php?with=' + datawith);
-};
-
-Contacts.prototype.refreshContacts = function() {
-  var that = this;
-
-  clearInterval(that.contactsInterval);
-
-  that.contactsInterval = setInterval(function() {
-    that.backend.getContacts().then(function(data) {
-      that.view.showContacts(data);
-    });
-  }, 300);
-};
 
 function ContactsModelView() {
 
@@ -174,6 +221,7 @@ ContactsView.prototype.showContacts = function(contacts) {
     $(this.contacts).html(html);
 
     this.contactList = $('.contacts li');
+    this.contactLinks = $('.contacts a');
   }
 }
 
@@ -184,43 +232,7 @@ function Conversation(backend, modelView, view) {
   this.view = view;
 
   this.messagesInterval;
-
-  this.view.messagebox.keydown(this.handleEnterKeyOnMessageForm.bind(this));
-  this.view.submit.click(this.handleClickOnMessageInput.bind(this));
-  this.view.messageform.submit(this.handleSubmitOnMessageForm.bind(this));
 }
-
-Conversation.prototype.handleEnterKeyOnMessageForm = function(e) {
-  if (e.ctrlKey && e.keyCode == 13) {
-    $(this.view.messagebox).val($(this.view.messagebox).val() + "\n");
-  } else if (e.keyCode == 13) {
-    e.preventDefault();
-
-    $(this.view.messageform).submit();
-    $(this.view.messagebox).val('');
-  }
-};
-
-Conversation.prototype.handleClickOnMessageInput = function(e) {
-  e.preventDefault();
-
-  $(this.view.messageform).submit();
-  $(this.view.messagebox).val('');
-};
-
-Conversation.prototype.handleSubmitOnMessageForm = function(e) {
-  e.preventDefault();
-
-  var to = $(this.view.messageform).attr('data-send-to'),
-      message =  $(this.view.messagebox).val(),
-      token = this.view.token;
-
-  this.backend.postMessage(to, message, token).then(function(data) {
-    // ...
-  });
-
-  return false;
-};
 
 Conversation.prototype.refreshMessages = function(datawith) {
   var that = this;
@@ -228,10 +240,12 @@ Conversation.prototype.refreshMessages = function(datawith) {
   clearInterval(that.messagesInterval);
 
   that.messagesInterval = setInterval(function() {
-    that.backend.getMessages(datawith).then(function(data) {
-      var newDataWith = $(this).attr('data-with');
+    var url = window.location.href;
 
-      if (newDataWith == datawith) {
+    that.backend.getMessages(datawith).then(function(data) {
+      var actualUrl = window.location.href;
+
+      if (url == actualUrl) {
         that.view.showMessages(data);
         that.view.moveMessagesToBottom();
         that.view.scrollDownMessages();
@@ -290,10 +304,10 @@ ConversationView.prototype.showMessages = function(messages) {
 
   $(this.messageblock).html(html);
 
-  $(this.messageblock.children('.content')).each(function() {
-    var text = $(this).text().replace('/\r\n/g', "<br>");
+  $(this.messageblock.find('.content')).each(function() {
+    var text = $(this).text().replace(/[\r\n]/g, "<br>");
 
-    $(this).text(text);
+    $(this).html(text);
   });
 
   this.messages = $('.message');
@@ -334,7 +348,7 @@ $(document).ready(function() {
 
   var controller = new Controller(contacts, conversation);
 
-  contacts.refreshContacts();
+  controller.refreshContacts();
 
   conversationView.moveMessagesToBottom();
   conversationView.scrollDownMessages();
